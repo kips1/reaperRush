3c59904d3ee4745bc6441a726029d45eb61d1009
SKITB11 <73229333+SKITB11@users.noreply.github.com>
2020-11-05 14:54:16 +0000

    Obstacle_Implementation

---
+++ Reaper Rush/Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour
+{
+    public GameObject obstacle;
+    Vector3 position;
+    bool next;
+    public float[] posX;
+    public float[] posZ;
+    int value = 1;
+    public int lastPosition = 1;
+    
+    void FixedUpdate()
+    {
+        StartCoroutine(WaitSys());
+    }
+
+    IEnumerator WaitSys()
+    {
+        yield return new WaitForSeconds(0.5f);
+        next = true;
+        Generate();
+    }
+
+    void Generate()
+    {
+        if (!next)
+            return;
+        int i = Random.Range(0, 3);
+        position.x = posX[i];
+        position.z += posZ[i];
+        GameObject obstacleClone = Instantiate(obstacle, position, obstacle.transform.rotation);
+        obstacleClone.GetComponent<ObstacleScript>().myNum = value;
+        obstacleClone.transform.SetParent(this.transform);
+        value += 1;
+        next = false;
+        return;
+    }
+
+    public void Message(int i)
+    {
+        if (lastPosition == i)
+        {
+            lastPosition += 1;
+            Debug.Log("Found");
+        } 
+
+        else
+        {
+            Debug.Log("Not found");
+        }
+    }
+}
