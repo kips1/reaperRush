d6daa0547a9861d3e79a1f45af248f49a37251f7
SKITB11 <73229333+SKITB11@users.noreply.github.com>
2020-12-02 13:41:14 +0000

    Added Player Health

--- Reaper Rush/Assets/Scripts/Player/Player.cs
+++ Reaper Rush/Assets/Scripts/Player/Player.cs
@@ -8,18 +8,31 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed = 25.0f;
     [SerializeField] private float gravity = 1.0f;
     [SerializeField] private float jumpHeight = 10.0f;
+
+
     private GameObject playerPosition;
     private CharacterController controller;
+    private HealthBar healthBar;
+    public Animator anim;
+
+
     private float yVelocity = 0.0f;
     private float xDirection = 0;
     private float zDirection = 1;
     public float distanceUnit;
+    public float maxHealth;
+    public float currentHealth;
+    public bool hasLost;
 
     // Start is called before the first frame update
     void Start()
     {
-    controller = GetComponent<CharacterController>();
-    InvokeRepeating("distance", 0, 1 / speed);
+        anim = GetComponent<Animator>();
+        maxHealth = 100;
+        currentHealth = 100;
+        hasLost = false;
+        controller = GetComponent<CharacterController>();
+        InvokeRepeating("distance", 0, 1 / speed);
     }
 
     // Update is called once per frame
@@ -33,6 +46,7 @@ public class Player : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 yVelocity = jumpHeight;
+                anim.SetBool("IsJumping", true);
             }
 
             else if (Input.GetKey(KeyCode.A) && xDirection > -4.48f)
@@ -55,14 +69,28 @@ public class Player : MonoBehaviour
             yVelocity -= gravity;
         }
 
+        if (currentHealth <= 0)
+        {
+            speed = 0;
+            distanceUnit += 0;
+            hasLost = true;
+        }
+
         velocity.y = yVelocity;
         controller.Move(velocity * Time.deltaTime);
     }
 
     void distance()
     {
-        distanceUnit = distanceUnit + 1;
+        if (currentHealth > 0)
+        {
+            distanceUnit = distanceUnit + 1;
+        } else
+        {
+            distanceUnit += 0;
+        }
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -70,9 +98,13 @@ public class Player : MonoBehaviour
         {
             CoinAddScript.coinAmount += 1;
             Destroy(other.gameObject);
-            
-
         }
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
     }
 
 }
