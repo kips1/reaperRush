53c3f9d982a98c718a505e37ed0fe39ba31831e9
B14cK3R <alexandruradeanublack@gmail.com>
2020-12-13 18:59:22 +0000

    Update 1.0.4

---
+++ Reaper Rush/Assets/Scripts/Power-Ups Generators/PowerUpHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpHealth : MonoBehaviour
+{
+    public GameObject PowerUp2Original;
+    public GameObject PowerUp2Container;
+
+    int z;
+    public int PowerUp;
+    int x;
+    void Start()
+    {
+        z = Random.Range(100, 500);
+
+
+        CreatePowerUp(PowerUp);
+
+    }
+    void CreatePowerUp(int coinsNum)
+    {
+        for (int i = 0; i < coinsNum; i++)
+        {
+            GameObject CoinClone = Instantiate(PowerUp2Original, new Vector3(Random.Range(-4, 4), PowerUp2Original.transform.position.y - 4, z += x = Random.Range(100, 200)), PowerUp2Original.transform.rotation);
+
+        }
+    }
+
+}
