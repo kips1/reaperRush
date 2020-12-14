d01a64b270c779c7a7023c4cac0290ab6781c6f0
kips-a <69807679+kips-a@users.noreply.github.com>
2020-12-14 01:21:43 +0000

    revert

--- Reaper Rush/Assets/Scripts/Multiplayer/GameManager.cs
+++ Reaper Rush/Assets/Scripts/Multiplayer/GameManager.cs
@@ -2,23 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GameManager : MonoBehaviourPunCallbacks
 {
     public int round;
-    public float distanceScore;
+    private int distanceScore;
     private int coinsCollected;
     public bool finalRound;
-
     private GameObject runner;
     private GameObject reaper;
     private GameObject rmController;
     private static GameManager _instance;
     public Photon.Realtime.Player s;
-
     public static GameManager Instance { get { return _instance; } }
-
-
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -30,7 +25,6 @@ public class GameManager : MonoBehaviourPunCallbacks
             _instance = this;
         }
     }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -39,6 +33,7 @@ public class GameManager : MonoBehaviourPunCallbacks
         round = 0;
         DontDestroyOnLoad(gameObject);
         s = PhotonNetwork.MasterClient;
+
     }
 
 
@@ -50,33 +45,38 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             Destroy(this.gameObject);
         }
-
         if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Game")
         {
-
             runner = GameObject.FindGameObjectWithTag("Player");
-            //reaper = GameObject.FindGameObjectWithTag("Reaper");
+            reaper = GameObject.FindGameObjectWithTag("Reaper");
             finalRound = true;
-
-                if (runner.GetComponent<Player>().hasLost && round == 0 && PhotonNetwork.IsMasterClient)
+            if (runner.GetComponent<Player>().hasLost && round == 0 && PhotonNetwork.IsMasterClient)
+            {
+                round++;
+                finalRound = true;
+                /*if (PhotonNetwork.IsMasterClient)
                 {
-                    round++;
-                    finalRound = true;
-                    StartCoroutine(ExecuteAfter(5.0f));
+                    runner.GetComponent<Player>().Reset();
+                    PhotonNetwork.Destroy(runner);
+                } else if (!PhotonNetwork.IsMasterClient)
+                {
+                    reaper.GetComponent<Reaper>().Reset();
+                    PhotonNetwork.Destroy(reaper);
+                    
+                }*/
+
+                PhotonNetwork.LoadLevel("RoleSwap");
+
+                StartCoroutine(ExecuteAfter(5.0f));
 
 
-                    //this.gameObject.tag = "mainManager";
-                }
-
+                //this.gameObject.tag = "mainManager";
+            }
             if (PhotonNetwork.IsMasterClient == false)
             {
                 round = 1;
             }
-
-
-
         }
-
         if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "RoleSwap" && round == 1)
         {
             if (PhotonNetwork.IsMasterClient && finalRound && PhotonNetwork.PlayerList.Length > 1)
@@ -84,24 +84,24 @@ public class GameManager : MonoBehaviourPunCallbacks
                 PhotonNetwork.SetMasterClient(PhotonNetwork.PlayerList[1]);
             }
             //PhotonNetwork.LoadLevel("Game");
-
-
             round++;
 
-        }
-            if (s != PhotonNetwork.MasterClient && round == 2)
-            {
-                PhotonNetwork.LoadLevel("Game");
-                round++;
-            }
-            else if (s != PhotonNetwork.MasterClient && round == 1 && PhotonNetwork.PlayerList.Length > 1)
-            {
-                PhotonNetwork.LoadLevel("Game");
-                round = 3;
-            }
-
 
         }
+        if (s != PhotonNetwork.MasterClient && round == 2)
+        {
+            PhotonNetwork.LoadLevel("Game");
+            round++;
+        }
+        else if (s != PhotonNetwork.MasterClient && round == 1)
+        {
+            PhotonNetwork.LoadLevel("Game");
+            round = 3;
+        }
+
+
+    }
+
     IEnumerator ExecuteAfter(float seconds)
     {
         yield return new WaitForSeconds(seconds);
