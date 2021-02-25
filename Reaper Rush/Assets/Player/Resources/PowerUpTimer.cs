251bc85aaab366c00aaa2e6468a1aae1b304ec8a
B14cK3R <alexandruradeanublack@gmail.com>
2021-02-25 21:51:56 +0000

    fixes
    Fixes

---
+++ Reaper Rush/Assets/Player/Resources/PowerUpTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class PowerUpTimer : Runner
+{
+    public float timeLeft = 3.0f;
+    public Text timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timer.text = (timeLeft).ToString("0");
+        if (timeLeft < 0.5)
+        {
+            timer.enabled = false;
+            timeLeft = 0;
+        }
+    }
+}
