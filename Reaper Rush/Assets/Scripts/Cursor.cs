ac70ec6580a6a459445b4b7caae897a3edb6066b
B14cK3R <alexandruradeanublack@gmail.com>
2021-03-27 20:34:03 +0000

    wtga
    asgag

---
+++ Reaper Rush/Assets/Scripts/Cursor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+
+    //public Texture2D cursorTexture;
+    //public CursorMode cursorMode = CursorMode.Auto;
+    //public Vector2 hotSpot = Vector2.zero;
+
+    public Texture2D cursorArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    private static void SetCursor(Texture2D cursorArrow, Vector2 zero, CursorMode forceSoftware)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+}
