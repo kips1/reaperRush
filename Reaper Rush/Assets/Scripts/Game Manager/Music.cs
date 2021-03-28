00eb1aefe6dc374c7eb821c98579a232518a78bb
kips-a <69807679+kips-a@users.noreply.github.com>
2021-03-28 15:36:00 +0100

    Options update

---
+++ Reaper Rush/Assets/Scripts/Game Manager/Music.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+
+    public GameObject options;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<AudioSource>().volume = GameObject.FindGameObjectWithTag("Options").GetComponent<Options>().backgroundmusicVol; ;   
+    }
+}
---
+++ Reaper Rush/Assets/Scripts/Game Manager/Options.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    private static Options _instance;
+    public float masterVol;
+    public float sfxVol;
+    public float backgroundmusicVol;
+    public static Options Instance { get { return _instance; } }
+    private void Awake()
+    {
+        // Ensures only one instance of this class is created
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = masterVol;
+    }
+}
