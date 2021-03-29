08a5ee94e2d216d499bedbecb06514e43a6a7c16
B14cK3R <alexandruradeanublack@gmail.com>
2021-03-29 23:35:50 +0100

    Updates

---
+++ Reaper Rush/Assets/Scripts/Player/CharacterSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelector : MonoBehaviour
+{
+    public int currentCharacterIndex;
+    public GameObject[] characters;
+    public string runnerName;
+
+
+
+    void Start()
+    {
+        currentCharacterIndex = PlayerPrefs.GetInt("SelectedCharacter", 0);
+        foreach (GameObject character in characters)
+            character.SetActive(false);
+
+        characters[currentCharacterIndex].SetActive(true);
+        runnerName = characters[currentCharacterIndex].name;
+        
+    }
+
+}
