1450680908bfff16aae2fb7d9b1c2587f8638213
SKITB11 <josh31966@outlook.com>
2021-03-31 13:32:11 +0100

    Updated Player Health for Invul Powerup

--- Reaper Rush/Assets/Scripts/UI/HealthBar.cs
+++ Reaper Rush/Assets/Scripts/UI/HealthBar.cs
@@ -17,12 +17,14 @@ public class HealthBar : MonoBehaviour
     private GameObject runner;
 
     public Image health;
+    public Image invul;
 
     // Start is called before the first frame update
     // Initialise fields
     private void Start()
     {
-        health = this.GetComponentInChildren<Image>();
+        health = GameObject.FindGameObjectWithTag("Health").GetComponent<Image>();
+        invul = GameObject.FindGameObjectWithTag("Invincibility").GetComponent<Image>();
         runner = GameObject.FindGameObjectWithTag("Player");
     }
 
@@ -32,5 +34,17 @@ public class HealthBar : MonoBehaviour
     {
         float healthPercentage = runner.GetComponent<Runner>().currentHealth / runner.GetComponent<Runner>().maxHealth;
         health.fillAmount = healthPercentage;
+
+        
+        if (!runner.GetComponent<Runner>().takeDamage)
+        {
+            health.gameObject.SetActive(false);
+            invul.gameObject.SetActive(true);
+        }
+        else
+        {
+            health.gameObject.SetActive(true);
+            invul.gameObject.SetActive(false);
+        }
     }
 }
